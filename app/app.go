ceb96a75017077b1ac678059025c64a9f56db67b
rugwirobaker <rugwirobaker@gmail.com>
2018-04-01 20:03:58 +0200

    MOVED: handler funcs to their own package

--- app/app.go
+++ app/app.go
@@ -2,13 +2,11 @@ package app
 
 import (
 	"database/sql"
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/rugwirobaker/structure/models"
+	"github.com/rugwirobaker/structure/handlers"
 )
 
 //App the core struct of the aoo
@@ -30,68 +28,7 @@ func (a *App) Run(addr string) {
 //Routes definition
 
 func (a *App) initRoutes() {
-	a.Router.HandleFunc("/articles", listArticles).Methods("GET")
-	a.Router.HandleFunc("/articles/{title}", getArticle).Methods("GET")
-	a.Router.HandleFunc("/articles", createArticle).Methods("POST")
-}
-
-//API represents dummy data
-type API struct {
-	//Message ...
-	Message string `json:"message"`
-}
-
-//Last to be implemented
-func getArticle(w http.ResponseWriter, r *http.Request) {
-	urlParams := mux.Vars(r)
-	title := urlParams["title"]
-	fmt.Println(title)
-	article := models.RetrieveArticle(title)
-
-	js, err := json.Marshal(article)
-	if err != nil {
-		panic(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(js)
-}
-
-func listArticles(w http.ResponseWriter, r *http.Request) {
-
-	articles := models.ListArticles()
-	//if err != nil {
-	//panic(err)
-	//}
-
-	js, err := json.Marshal(articles)
-	if err != nil {
-		panic(err)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(js)
-}
-
-var id int
-
-func createArticle(w http.ResponseWriter, r *http.Request) {
-
-	var article models.Article
-	err := json.NewDecoder(r.Body).Decode(&article)
-	if err != nil {
-		panic(err)
-	}
-
-	models.CreateArticle(article)
-
-	js, err := json.Marshal(article)
-	if err != nil {
-		panic(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	w.Write(js)
+	a.Router.HandleFunc("/articles", handlers.ListArticles).Methods("GET")
+	a.Router.HandleFunc("/articles/{title}", handlers.RetrieveArticle).Methods("GET")
+	a.Router.HandleFunc("/articles", handlers.CreateArticle).Methods("POST")
 }
---
+++ handlers/handlers.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rugwirobaker/structure/models"
+)
+
+// RetrieveArticle ...
+func RetrieveArticle(w http.ResponseWriter, r *http.Request) {
+	urlParams := mux.Vars(r)
+	title := urlParams["title"]
+	fmt.Println(title)
+	article := models.RetrieveArticle(title)
+
+	js, err := json.Marshal(article)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(js)
+}
+
+// ListArticles ...
+func ListArticles(w http.ResponseWriter, r *http.Request) {
+
+	articles := models.ListArticles()
+	//if err != nil {
+	//panic(err)
+	//}
+
+	js, err := json.Marshal(articles)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(js)
+}
+
+var id int
+
+//CreateArticle ...
+func CreateArticle(w http.ResponseWriter, r *http.Request) {
+
+	var article models.Article
+	err := json.NewDecoder(r.Body).Decode(&article)
+	if err != nil {
+		panic(err)
+	}
+
+	models.CreateArticle(article)
+
+	js, err := json.Marshal(article)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(js)
+}
