ee94a4c23a8a9ecd85d86c543a3c719e2634203d
rugwirobaker <rugwirobaker@gmail.com>
2018-04-03 15:50:42 +0200

    ADDED: database connection

--- app/app.go
+++ app/app.go
@@ -2,8 +2,11 @@ package app
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	// Database driver
+	_ "github.com/lib/pq"
 
 	"github.com/gorilla/mux"
 	"github.com/rugwirobaker/structure/handlers"
@@ -12,11 +15,12 @@ import (
 //App the core struct of the aoo
 type App struct {
 	Router mux.Router
-	DB     sql.DB
+	DB     *sql.DB
 }
 
 //Initialize ...
 func (a *App) Initialize() {
+	a.initDb(Host, User, Password, Dbname, Port)
 	a.initRoutes()
 }
 
@@ -31,4 +35,26 @@ func (a *App) initRoutes() {
 	a.Router.HandleFunc("/articles", handlers.ListArticles).Methods("GET")
 	a.Router.HandleFunc("/articles/{title}", handlers.RetrieveArticle).Methods("GET")
 	a.Router.HandleFunc("/articles", handlers.CreateArticle).Methods("POST")
+}
+
+//Initialize database connection
+func (a *App) initDb(host, username, password, dbname string, port int) {
+	//TODO: fix connection string
+	connString := fmt.Sprintf("host=%s port=%d user=%s password=%s "+
+		"dbname=%s sslmode=disable", host, port, username, password, dbname)
+
+	var err error
+
+	a.DB, err = sql.Open("postgres", connString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//check if the datasource is valid
+	err = a.DB.Ping()
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Connection Established")
+	}
 }
