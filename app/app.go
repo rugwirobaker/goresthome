fd98c05688897ceda2bfedf35e42f97044b9a756
rugwirobaker <rugwirobaker@gmail.com>
2018-04-05 12:00:31 +0200

    MOVED: handlers to their own package successfully

--- app/app.go
+++ app/app.go
@@ -2,16 +2,14 @@ package app
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 	// Database driver
 	_ "github.com/lib/pq"
+	"github.com/rugwirobaker/structure/handlers"
 
 	"github.com/gorilla/mux"
-	"github.com/rugwirobaker/structure/models"
 )
 
 //App the core struct of the aoo
@@ -35,9 +33,21 @@ func (a *App) Run(addr string) {
 
 func (a *App) initRoutes() {
 	a.Router.StrictSlash(false)
-	a.Router.HandleFunc("/articles", a.RetrieveArticles).Methods("GET")
-	a.Router.HandleFunc("/articles/{id:[0-9]+}", a.RetrieveArticle).Methods("GET")
-	a.Router.HandleFunc("/articles", a.CreateArticle).Methods("POST")
+
+	a.Router.HandleFunc("/articles", func(w http.ResponseWriter,
+		r *http.Request) {
+		handlers.RetrieveArticles(w, r, a.DB)
+	}).Methods("GET")
+
+	a.Router.HandleFunc("/articles/{id:[0-9]+}", func(w http.ResponseWriter,
+		r *http.Request) {
+		handlers.RetrieveArticle(w, r, a.DB)
+	}).Methods("GET")
+
+	a.Router.HandleFunc("/articles", func(w http.ResponseWriter,
+		r *http.Request) {
+		handlers.CreateArticle(w, r, a.DB)
+	}).Methods("POST")
 }
 
 //Initialize database connection
@@ -64,71 +74,3 @@ func (a *App) initDb(host, username, password, dbname string, port int) {
 
 //handlers
 //TODO: find a way to move handlers to their own package
-
-//CreateArticle ...
-func (a *App) CreateArticle(w http.ResponseWriter, r *http.Request) {
-
-	var article models.Article
-	err := json.NewDecoder(r.Body).Decode(&article)
-	if err != nil {
-		panic(err)
-	}
-
-	article.CreateArticle(a.DB)
-
-	js, err := json.Marshal(article)
-	if err != nil {
-		panic(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	w.Write(js)
-}
-
-//RetrieveArticle ...
-func (a *App) RetrieveArticle(w http.ResponseWriter, r *http.Request) {
-	urlParams := mux.Vars(r)
-	id, err := strconv.Atoi(urlParams["id"])
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	article := models.Article{ID: id}
-
-	article.RetrieveArticle(a.DB)
-	js, err := json.Marshal(article)
-	if err != nil {
-		panic(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(js)
-
-}
-
-//RetrieveArticles ...
-func (a *App) RetrieveArticles(w http.ResponseWriter, r *http.Request) {
-	//count, _ := strconv.Atoi(r.FormValue("count"))
-	//start, _ := strconv.Atoi(r.FormValue("start"))
-
-	//if count > 10 || count < 1 {
-	//count = 10
-	//}
-	//if start < 0 {
-	//start = 0
-	//}
-
-	articles := models.ListArticles(a.DB)
-	js, err := json.Marshal(articles)
-	if err != nil {
-		panic(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(js)
-
-}
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,37 +1,81 @@
 package handlers
 
-// RetrieveArticle ...
-//func RetrieveArticle(w http.ResponseWriter, r *http.Request) {
-//	urlParams := mux.Vars(r)
-//	title := urlParams["title"]
-//fmt.Println(title)
-//	article := models.RetrieveArticle(title)
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
 
-//	js, err := json.Marshal(article)
-//	if err != nil {
-//		panic(err)
-//	}
-
-//	w.Header().Set("Content-Type", "application/json")
-//	w.WriteHeader(http.StatusOK)
-//	w.Write(js)
-//}
-
-// ListArticles ...
-//func ListArticles(w http.ResponseWriter, r *http.Request) {
-
-//	articles := models.ListArticles()
-//if err != nil {
-//panic(err)
-//}
-
-//	js, err := json.Marshal(articles)
-//	if err != nil {
-//		panic(err)
-//	}
-//	w.Header().Set("Content-Type", "application/json")
-//	w.WriteHeader(http.StatusOK)
-//	w.Write(js)
-//}
+	"github.com/gorilla/mux"
+	"github.com/rugwirobaker/structure/models"
+)
 
 //CreateArticle ...
+func CreateArticle(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+
+	var article models.Article
+	err := json.NewDecoder(r.Body).Decode(&article)
+	if err != nil {
+		panic(err)
+	}
+
+	article.CreateArticle(db)
+
+	js, err := json.Marshal(article)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	w.Write(js)
+}
+
+//RetrieveArticle ...
+func RetrieveArticle(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	urlParams := mux.Vars(r)
+	id, err := strconv.Atoi(urlParams["id"])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	article := models.Article{ID: id}
+
+	article.RetrieveArticle(db)
+	js, err := json.Marshal(article)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(js)
+
+}
+
+//RetrieveArticles ...
+func RetrieveArticles(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	//count, _ := strconv.Atoi(r.FormValue("count"))
+	//start, _ := strconv.Atoi(r.FormValue("start"))
+
+	//if count > 10 || count < 1 {
+	//count = 10
+	//}
+	//if start < 0 {
+	//start = 0
+	//}
+
+	articles := models.ListArticles(db)
+	js, err := json.Marshal(articles)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(js)
+
+}
