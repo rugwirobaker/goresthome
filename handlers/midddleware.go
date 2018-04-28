6d88416b91adf4529db8793d086fe828b4f6e7ff
rugwirobaker <rugwirobaker@gmail.com>
2018-04-28 17:33:44 +0200

    MODIFIED: logging middleware/ wrapped http.ResponseWriter

--- handlers/midddleware.go
+++ handlers/midddleware.go
@@ -6,15 +6,43 @@ import (
 	"net/http"
 )
 
+//LogResponseWriter wraps the builtin net/http ResponseWriter
+// for response logging purposes
+type LogResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+//NewLogResponseWriter creates a new loggingResponse type
+func NewLogResponseWriter(w http.ResponseWriter) *LogResponseWriter {
+	return &LogResponseWriter{w, http.StatusOK}
+}
+
+//WriteHeader initializes loggingResponseWriter values
+func (lrw *LogResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
 //LoggingHandler ...
 func LoggingHandler(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	logger := fmt.Sprintf("*** request: %s | %s%s", r.Method, r.Host, r.URL)
 	log.Println(logger)
 
+	lrw := NewLogResponseWriter(w)
+
 	//set application wide http response headers
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Cache-Control", "no-cache")
-	next(w, r)
+
+	next(lrw, r)
+
+	statusCode := lrw.statusCode
+	logger = fmt.Sprintf("%d %s", statusCode, http.StatusText(statusCode))
+	log.Println(logger)
+
 }
 
 func cachingHandler(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {}
+
+//func setHeadersHandler(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {}
