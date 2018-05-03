b9dc7884a71d8bf907b8769b0f81c216c0e313a1
rugwirobaker <rugwirobaker@gmail.com>
2018-05-03 19:54:41 +0200

    ADDED: user model struct

--- models/models.go
+++ models/models.go
@@ -1,17 +1,42 @@
+//Package models Define interface for storage of different application components
 package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
-//Article ...
+//Article models the database entity article
 type Article struct {
 	ID        int       `json:"id"`
 	Title     string    `json:"title"`
 	Body      string    `json:"body"`
 	Author    string    `json:"author"`
 	CreatedOn time.Time `json:"createdon"`
+}
+
+//ArticleResults models a list of retrieved articles
+type ArticleResults struct {
+	Count    int       `json:"article_count"`
+	Articles []Article `json:"articles"`
+}
+
+//User models the database entity user
+type User struct {
+	ID         int       `json:"id,omitempty"`
+	Fname      string    `json:"first_name"`
+	Lname      string    `json:"last_name"`
+	Email      string    `json:"email"`
+	Pass       string    `json:"passwd"`
+	PassHash   []byte    `json:"passwdhash,omitempty"`
+	DateJoined time.Time `json:"joinedOn"`
+}
+
+//Users models a collection of user objects
+type Users struct {
+	Users []User `json:"users"`
+	Count int    `json:"user_count"`
 }
 
 //var id int
@@ -56,14 +81,8 @@ func (c *Article) DeleteArticle(db *sql.DB) error {
 	return nil
 }
 
-// ArticleResults models a list of retrieved articles
-type ArticleResults struct {
-	Number   int       `json:"article_number"`
-	Articles []Article `json:"articles"`
-}
-
-func (s *ArticleResults) getNumber() {
-	s.Number = len(s.Articles)
+func (s *ArticleResults) getCount() {
+	s.Count = len(s.Articles)
 }
 
 //ListArticles ...
@@ -81,7 +100,72 @@ func (s *ArticleResults) ListArticles(db *sql.DB) error {
 			return err
 		}
 		s.Articles = append(s.Articles, c)
-		s.getNumber()
+		s.getCount()
 	}
 	return nil
+}
+
+//RegisterUser creates an new user instance
+func (u *User) RegisterUser(db *sql.DB) error {
+	u.DateJoined = time.Now()
+	err := db.QueryRow("INSERT INTO users(fname, lname, email,"+
+		"passhash, datejoined) VALUES($1, $2, $3, $4, $5,) RETURNING id",
+		u.Fname, u.Lname, u.Email, u.PassHash, u.DateJoined).Scan(&u.ID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//DeleteUser deletes user with dd:ID
+func (u *User) DeleteUser(db *sql.DB) error {
+	err := db.QueryRow("DELETE FROM users WHERE id=$1 "+
+		"RETURNING id", u.ID).Scan(&u.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//UpdateUser update a user entity information
+func (u *User) UpdateUser(db *sql.DB) error {
+	return errors.New(" RetrieveUserByEmail Not implemented")
+}
+
+//RetrieveUserByEmail queries user with a given Email from the database
+func (u *User) RetrieveUserByEmail(db *sql.DB) error {
+	err := db.QueryRow("SELECT id, email, passhash FROM users WHERE email=$1",
+		u.Email).Scan(&u.ID, &u.Email, &u.PassHash)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//ListUsers queries a list of user entities
+func (us *Users) ListUsers(db *sql.DB) error {
+	rows, err := db.Query("SELECT id, title, author FROM users")
+
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var u User
+		if err := rows.Scan(&u.ID, &u.Email, &u.DateJoined); err != nil {
+			return err
+		}
+		us.Users = append(us.Users, u)
+		us.getCount()
+	}
+
+	return nil
+}
+
+func (us *Users) getCount() {
+	us.Count = len(us.Users)
 }
